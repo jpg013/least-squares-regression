4e1b34e5104aff20bc7dca884f739340711fb2cc
Justin Graber <jpg013@gmail.com>
2018-12-14 20:50:46 -0600

    functions for calculating slope/intercept and line equation.

--- least-squares.js
+++ least-squares.js
@@ -1,21 +1,69 @@
-function leastSquares(points=[]) {
-  if (!points || !Array.isArray(points)) {
+function compose(...fns) {
+  return fns.reduce((f, g) => (...args) => f(g(...args)));
+};
+
+const sumDataPoints = (data=[]) => {
+  if (!data || !Array.isArray(data)) {
     throw new TypeError('least squares requires an array argument of { x, y } data points.');
   }
 
-  const sums = points.reduce((acc, curr) => {
+  return data.reduce((acc, curr) => {
     const x = parseInt(curr && curr.x);
     const y = parseInt(curr && curr.y);
 
     return {
       x: acc.x + (isNaN(x) ? 0 : x),
-      y: acc.y + (isNaN(y) ? 0 : x),
+      y: acc.y + (isNaN(y) ? 0 : y),
       xx: acc.xx + (isNaN(x) ? 0 : Math.pow(x, 2)),
       xy: acc.xy + ((isNaN(x) || isNaN(y)) ? 0 : x * y),
+      idx: acc.idx + 1,
     };
-  }, { x: 0, y: 0, xx: 0, xy: 0 });
-
-  return sums;
+  }, { x: 0, y: 0, xx: 0, xy: 0, idx: 0 });
 }
 
-exports.leastSquares = leastSquares;
+const calculateSlope = (data={}) => {
+  const num = (data.idx * data.xy) - (data.x * data.y);
+  const den = (data.idx * (data.xx)) - (Math.pow(data.x, 2));
+
+  return {
+    ...data,
+    slope: (num / den)
+  };
+}
+
+const calculateIntercept = (data={}) => {
+  const intercept = (data.y - (data.slope * data.x)) / data.idx;
+
+  return {
+    ...data,
+    intercept,
+  }
+};
+
+const assembleLineEquation = (data={}) => {
+  return {
+    ...data,
+    lineEquation: x => (data.slope * data.x) + data.intercept
+  }
+}
+
+const parseVal = (data={}) => {
+  return {
+    slope: data.slope,
+    intercept: data.intercept,
+    lineEquation: data.lineEquation,
+  }
+}
+
+exports.leastSquares = compose(
+  parseVal,
+  assembleLineEquation,
+  calculateIntercept,
+  calculateSlope,
+  sumDataPoints,
+);
+exports.assembleLineEquation = assembleLineEquation;
+exports.parseVal = parseVal;
+exports.sumDataPoints = sumDataPoints;
+exports.calculateSlope = calculateSlope;
+exports.calculateIntercept = calculateIntercept;
--- least-squares.test.js
+++ least-squares.test.js
@@ -1,8 +1,24 @@
 const { assert }       = require('chai');
 const { leastSquares } = require('./least-squares');
 
+const dataSet = [
+  { x: 2, y: 4, },
+  { x: 3, y: 5, },
+  { x: 5, y: 7, },
+  { x: 7, y: 10, },
+  { x: 9, y: 15, },
+]
+
 describe('leastSquares', () => {
   it('exports', () => {
     assert.isFunction(leastSquares);
+  });
+
+  it('calculates the correct value', () => {
+    const val = leastSquares(dataSet);
+
+    assert.equal(val.slope.toFixed(4), 1.5183);
+    assert.equal(val.intercept.toFixed(4), 0.3049);
+    assert.isFunction(val.lineEquation);
   });
 });
